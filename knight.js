9a6e0fcdb5d1122531421c2f3406fbf9cf5bfec2
vaasut <vaasu@Vaasus-MacBook-Air.local>
2018-02-08 20:19:15 -0500

    knight moves

--- knight.js
+++ knight.js
@@ -2,4 +2,37 @@ class Knight extends Piece {
 	constructor(rank,file,color,type,state){
 		super(rank,file,color,type,state);
 	}
+	updatestate(game){
+	}
+	getmoves(game){
+		let moves =[];
+		moves.push([7-this.rank-2,this.file+1]);
+		moves.push([7-this.rank-2,this.file-1]);
+
+		moves.push([7-this.rank+2,this.file+1]);
+		moves.push([7-this.rank+2,this.file-1]);
+
+		moves.push([7-this.rank+1,this.file+2]);
+		moves.push([7-this.rank-1,this.file+2]);
+
+		moves.push([7-this.rank+1,this.file-2]);
+		moves.push([7-this.rank-1,this.file-2]);
+
+		let corrected_moves = [];
+
+		for (let i in moves){
+			let move = moves[i];
+			console.log("x: "+ move[0]);
+			if (move[0] > -1 && move[0] < 8 && move[1] > -1 && move[1] < 8){
+				if (game[7-move[0]][move[1]] === undefined){
+					corrected_moves.push(moves[i]);
+				}
+				else if (game[7-move[0]][move[1]].color != this.color){
+					corrected_moves.push(moves[i]);
+				}
+			}
+		}
+
+		return corrected_moves;
+	}
 }
--- pawn.js
+++ pawn.js
@@ -13,11 +13,12 @@ class Pawn extends Piece {
 		moves = []
 		if (this.color == 0){
 			if (this.state == 0){ //unmoved
-				moves.push([7-this.rank-2,this.file])
+				if (game[this.rank+1][this.file] === "" && game[this.rank+2][this.file] === ""){
+					moves.push([7-this.rank-2,this.file]);
+				}
 			}
 
 			if (game[this.rank+1][this.file] === ""){
-				//console.log('apple');
 				moves.push([7-this.rank-1,this.file]);
 			}
 			
@@ -30,6 +31,30 @@ class Pawn extends Piece {
 			if (game[this.rank+1][this.file-1]){
 				if (game[this.rank+1][this.file-1].color == 1){
 					moves.push([7-this.rank-1,this.file-1]);
+				}
+			}
+			return moves
+		}
+		else{
+			if (this.state == 0){ //unmoved
+				if (game[this.rank-1][this.file] === "" && game[this.rank-2][this.file] === ""){
+					moves.push([7-this.rank+2,this.file]);
+				}
+			}
+
+			if (game[this.rank-1][this.file] === ""){
+				moves.push([7-this.rank+1,this.file]);
+			}
+			
+			if (game[this.rank-1][this.file+1]){
+				if (game[this.rank-1][this.file+1].color == 0){
+					moves.push([7-this.rank+1,this.file+1]);
+				}
+			}
+
+			if (game[this.rank-1][this.file-1]){
+				if (game[this.rank-1][this.file-1].color == 0){
+					moves.push([7-this.rank+1,this.file-1]);
 				}
 			}
 			return moves
